2ef2b98634c46889d0fb6a7a773601e7cffeaed9
RobTabLap <zygar84@gmail.com>
2016-06-08 16:46:13 -0400

    Created Android Project

---
+++ app/src/main/java/com/example/zergd/sea/MainGame.java
@@ -0,0 +1,40 @@
+package com.example.zergd.sea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.*;
+
+public class MainGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+        TextView text = (TextView)findViewById(R.id.DispWin);
+        Thread thr = new Thread(mTask);
+        thr.start();
+    }
+
+    Runnable mTask = new Runnable() {
+        public void run() {
+            // just sleep for 30 seconds.
+            for (int i=0;i<100;i++) {
+                try {
+                    Thread.sleep(1000);
+                    runOnUiThread(done);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    Runnable done = new Runnable() {
+        public void run() {
+            TextView text = (TextView)findViewById(R.id.DispWin);
+            text.append("\nTesting 123------------Testing 123--------BEEP");
+        }
+    };
+}
