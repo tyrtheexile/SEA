596b44adfd464afc3b17016e5ec6d5e924dce880
RobWork <RobWork>
2016-06-20 15:10:45 -0400

    Load added? Fragment Testing

--- app/src/main/java/com/example/zergd/sea/astroFrag.java
+++ app/src/main/java/com/example/zergd/sea/astroFrag.java
@@ -3,24 +3,16 @@ package com.example.zergd.sea;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
+//import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link astroFrag.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link astroFrag#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class astroFrag extends Fragment {
-    //@Override
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.astro_frag, container, false);
