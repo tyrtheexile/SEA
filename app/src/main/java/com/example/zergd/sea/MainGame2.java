9f50a85edce5c7bea64340238d7a835d5391621a
RobWork <RobWork>
2016-06-22 23:54:55 -0400

    Load added? Fragment Testing

--- app/src/main/java/com/example/zergd/sea/MainGame2.java
+++ app/src/main/java/com/example/zergd/sea/MainGame2.java
@@ -145,6 +145,7 @@ public class MainGame2 extends Activity {
                 ex.printStackTrace();
                 Log.e("Test",Log.getStackTraceString(ex));
             }
+            Log.e("Test","End of Load in"+astro.getName());
 
         }
         else {
