0f6ca463df2f35f8629a609643a75d4c7ac152eb
RobWork <RobWork>
2016-06-23 22:14:00 -0400

    Working on Synth Menu

--- app/src/main/java/com/example/zergd/sea/MainGame2.java
+++ app/src/main/java/com/example/zergd/sea/MainGame2.java
@@ -6,6 +6,7 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.*;
+import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
