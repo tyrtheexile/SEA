21fbbd2efedd2d7a7dac484abd0467aa976a7084
RobWork <RobWork>
2016-06-28 13:16:13 -0700

    Adding new Text System

--- app/src/main/java/com/example/zergd/sea/MainActivity.java
+++ app/src/main/java/com/example/zergd/sea/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Global.set_Debug(1);
         Global.setImmortal(false);
         Global.setTimeIncrement(100);
-        Global.setTestmode(2);
+        Global.setTestmode(1);
 
         Button startGame = (Button) this.findViewById(R.id.StartGame);
         Button newGame = (Button) this.findViewById(R.id.newGame);
